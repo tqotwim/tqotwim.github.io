bda83e411806e22720962dc432baed31d78f57af
tqotwim <tqotwim@users.noreply.github.com>
2019-07-25 04:19:18 -0400

    Uploading check in log
    Beginning of check in log for agency

---
+++ check_in_logs/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// AddClientButton onclick should duplicate first and last name text box and position new text boxes under previous
+// Submit button should send data to appropriate worker in check out logs and also to page with percentages, daily numbers
+//Submit should send client data to alphabetical page by last name for client to see client activity in agency
+const displayCheckedInClient = {
+  
+}  
+let addClientButton = document.getElementbyId('addClientButton');
+//  addClientButton.addEventListener('click', function(){
+//    addClient();
+//  });
+const addClient = function() {
+  
+};
+  
+ 
